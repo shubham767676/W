b607fffaed5f06daff8e0e6e6aeb6213e073fd8b
“shubham767676” <shubhamv.singh06@gmail.com>
2022-03-14 05:37:52 +0530

    Added: Welcome Message

---
+++ main/java/com/bridgelabz/HotelReservationProgram/HotelReservation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.HotelReservationProgram;
+
+public class HotelReservation {
+
+	// static Hotel hotel = new Hotel("Shubham", 1000, 2000);
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		// System.out.println(hotel);
+
+	}
+
+}
