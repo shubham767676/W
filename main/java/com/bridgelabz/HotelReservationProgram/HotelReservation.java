df755a3553689d78a42956b7e580a8283928642f
“shubham767676” <shubhamv.singh06@gmail.com>
2022-03-14 06:19:10 +0530

    Added: Refactoed Hotel Added

--- main/java/com/bridgelabz/HotelReservationProgram/HotelReservation.java
+++ main/java/com/bridgelabz/HotelReservationProgram/HotelReservation.java
@@ -2,11 +2,11 @@ package com.bridgelabz.HotelReservationProgram;
 
 public class HotelReservation {
 
-	// static Hotel hotel = new Hotel("Shubham", 1000, 2000);
+	static Hotel hotel = new Hotel("Shubham", 1000, 2000);
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-		// System.out.println(hotel);
+		System.out.println(hotel);
 
 	}
 
